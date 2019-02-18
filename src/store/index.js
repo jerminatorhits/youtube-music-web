0f71f06a16210999a4ee2d5d0e57b5ad0f7188db
Jeremy Kwa <jeremykwa@gmail.com>
2019-02-18 11:27:42 -0800

    reinstate logger

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 
-// import { createLogger } from "redux-logger"
+import { createLogger } from "redux-logger"
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
 
@@ -18,7 +18,7 @@ const persistConfig = {
 
 let middleware = [ promise(), thunk ]
 if (process.env.NODE_ENV !== 'production') {
-  middleware = [ ...middleware ]
+  middleware = [ ...middleware, createLogger() ]
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
